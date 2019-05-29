b74d398530194dc5cc95e2acd8fd309bdabcff36
chinodt <tineo_rangel_david@hotmail.com>
2019-05-29 17:12:04 -0400

    Subida de codigo

---
+++ TestLectoraCodigo/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace TestLectoraCodigo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtContent = new System.Windows.Forms.TextBox();
+            this.cmdClear = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cboPuerto = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // txtContent
+            // 
+            this.txtContent.Location = new System.Drawing.Point(12, 50);
+            this.txtContent.Multiline = true;
+            this.txtContent.Name = "txtContent";
+            this.txtContent.Size = new System.Drawing.Size(510, 133);
+            this.txtContent.TabIndex = 0;
+            // 
+            // cmdClear
+            // 
+            this.cmdClear.Location = new System.Drawing.Point(447, 189);
+            this.cmdClear.Name = "cmdClear";
+            this.cmdClear.Size = new System.Drawing.Size(75, 23);
+            this.cmdClear.TabIndex = 1;
+            this.cmdClear.Text = "Clear";
+            this.cmdClear.UseVisualStyleBackColor = true;
+            this.cmdClear.Click += new System.EventHandler(this.cmdClear_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Introduzca Puerto";
+            // 
+            // cboPuerto
+            // 
+            this.cboPuerto.FormattingEnabled = true;
+            this.cboPuerto.Location = new System.Drawing.Point(106, 19);
+            this.cboPuerto.Name = "cboPuerto";
+            this.cboPuerto.Size = new System.Drawing.Size(121, 21);
+            this.cboPuerto.TabIndex = 3;
+            this.cboPuerto.SelectedIndexChanged += new System.EventHandler(this.cboPuerto_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(534, 222);
+            this.Controls.Add(this.cboPuerto);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmdClear);
+            this.Controls.Add(this.txtContent);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtContent;
+        private System.Windows.Forms.Button cmdClear;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cboPuerto;
+    }
+}
+
---
+++ TestLectoraCodigo/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace TestLectoraCodigo
+{
+    public partial class Form1 : Form
+    {
+        delegate void SetTextCallback(string text);
+        SerialPort ComPort = new SerialPort();
+        [DllImport("KERNEL32.DLL", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.StdCall, SetLastError = true)]
+        public static extern uint WinExec(string lpCmdLine, uint uCmdShow);
+
+        public Form1()
+        {
+
+            InitializeComponent();
+            fillComboPorts();
+            habiltaPort();
+
+        }
+
+        private void habiltaPort()
+        {
+            if (this.cboPuerto.Text != "")
+            {
+                ComPort.PortName = this.cboPuerto.Text;
+                ComPort.BaudRate = 9600;
+                ComPort.DataBits = 7;
+                Thread.Sleep(1000);
+                ComPort.DtrEnable = true;
+                ComPort.Open();
+                ComPort.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(port_DataReceived_1);
+            }
+
+        }
+
+        private void cmdClear_Click(object sender, EventArgs e)
+        {
+            this.txtContent.Text = "";
+        }
+
+        private void SetText(string text)
+        {
+            this.txtContent.Text += text;
+        }
+
+        private void port_DataReceived_1(object sender, SerialDataReceivedEventArgs e)
+        {
+            Thread.Sleep(1000);
+            string InputData = String.Empty;
+            InputData = ComPort.ReadExisting();
+            if (InputData != String.Empty)
+            {
+                this.BeginInvoke(new SetTextCallback(SetText), new object[] { InputData });
+            }
+        }
+
+        private void fillComboPorts()
+        {
+            string[] ports = SerialPort.GetPortNames();
+            foreach (string port in ports)
+            {
+                csPorts csP = new csPorts();
+                csP.Text = port;
+                csP.Value = port;
+                cboPuerto.Items.Add(csP);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cboPuerto_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            habiltaPort();
+        }
+    }
+
+    public class csPorts
+    {
+        public string Text { get; set; }
+        public string Value { get; set; }
+
+        public override string ToString()
+        {
+            return Text;
+        }
+    }
+}
